14c28dd3317bb44823d0ce1e4647814c7dd19bcf
Felipe Lucena <76856707+felipelucena098@users.noreply.github.com>
2022-12-20 12:06:25 -0300

    feat: estruturacao para api

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
 import { Container } from './styled';
 
 import Header from '../../components/Header';
 
+import api from '../../api';
+
 export default () => {
     const history = useHistory();
 
+    useEffect(() => {
+        const getCategories = async () => {
+            const categories = await api.categories();
+        };
+        getCategories();
+    }, []);
+
+    const [headerSearch, setHeaderSearch] = useState('');
+
     return (
         <Container>
-            <Header />
+            <Header search={headerSearch} onSearch={setHeaderSearch} />
         </Container>
     );
 }
